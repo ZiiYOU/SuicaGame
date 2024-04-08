adc9b334f0b6be0802313faa7f0083c61a64ff5b
Jiyoung <jiyoung990602@gmail.com>
2024-04-08 11:00:00 +0900

    ✨ update : 과일 setting

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { Bodies, Engine, Render, Runner, World } from "matter-js";
+import { FRUITS } from "./fruits";
 
 const engine = Engine.create();
 const render = Render.create({
@@ -7,7 +8,7 @@ const render = Render.create({
   options: {
     wireframes: false,
     background: "#F7F4C8",
-    width: 620,
+    width: 680,
     height: 750,
   },
 });
@@ -19,17 +20,17 @@ const leftWall = Bodies.rectangle(15, 350, 30, 740, {
   render: { fillStyle: "#E6B143" },
 });
 
-const rightWall = Bodies.rectangle(605, 350, 30, 740, {
+const rightWall = Bodies.rectangle(665, 350, 30, 740, {
   isStatic: true,
   render: { fillStyle: "#E6B143" },
 });
 
-const ground = Bodies.rectangle(310, 720, 620, 60, {
+const ground = Bodies.rectangle(310, 720, 740, 60, {
   isStatic: true,
   render: { fillStyle: "#E6B143" },
 });
 
-const topLine = Bodies.rectangle(310, 130, 620, 2, {
+const topLine = Bodies.rectangle(310, 130, 740, 2, {
   isStatic: true,
   render: { fillStyle: "E6B143" },
 });
@@ -38,3 +39,21 @@ World.add(world, [leftWall, ground, topLine, rightWall]);
 
 Render.run(render);
 Runner.run(engine);
+
+function addFruit() {
+  const index = Math.floor(Math.random() * 5);
+  const fruit = FRUITS[index];
+
+  const body = Bodies.circle(300, 50, fruit.radius, {
+    index: index,
+    isSleeping: true,
+    render: {
+      sprite: { texture: `${fruit.name}.png` },
+    },
+    restitution: 0.2,
+  });
+
+  World.add(world, body);
+}
+
+addFruit();
